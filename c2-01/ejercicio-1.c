083aa1875bf4a054bd15a1017799ffda23b17797
José Daniel Valencia M. <josedanielvalencia019@gmail.com>
2025-04-05 19:43:40 -0500

    ejercicio-1.c

---
+++ c2-01/ejercicio-1.c
@@ -0,0 +1,30 @@
+/*
+ * Nombre: [Tu nombre]
+ * Integrantes: [Nombres del equipo]
+ * Ejercicio 1: [Resumen del enunciado]
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola mundo\n");
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Crear carpeta c2-01 y agregar ejercicio-1.c
