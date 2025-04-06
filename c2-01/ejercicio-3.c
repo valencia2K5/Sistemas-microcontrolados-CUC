80d402e7432dd3bb3fc87895a89fcebf76d59e9a
José Daniel Valencia M. <josedanielvalencia019@gmail.com>
2025-04-05 19:48:49 -0500

    ejercicio-3.c

---
+++ c2-01/ejercicio-3.c
@@ -0,0 +1,12 @@
+/*
+ * Nombre: [Tu nombre]
+ * Integrantes: [Nombres del equipo]
+ * Ejercicio 1: [Resumen del enunciado]
+ */
+
+#include <stdio.h>
+
+int main() {
+    printf("Hola mundo\n");
+    return 0;
+}
